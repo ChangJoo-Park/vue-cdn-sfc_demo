12503fe2b14dc8d65b70e390bfa3878639cf91ed
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-03-10 11:49:55 +0900

    Update for vue 3

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    THIS IS MY COMPONENT
+    <div>
+      {{ name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-component',
+  data () {
+    return {
+      name: "ABCD"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- entry.js
+++
@@ -1,2 +0,0 @@
-import Component from './src/example.vue';
-Vue.component(Component.name, Component);
